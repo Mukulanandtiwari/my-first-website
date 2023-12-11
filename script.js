e4c719bfb097cc10c842bdb6c549b9f62d7ee8ad
Mukulanandtiwari <124246223+Mukulanandtiwari@users.noreply.github.com>
2023-12-11 17:49:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,107 +3,106 @@ document.addEventListener('DOMContentLoaded', function () {
     const toggleBtnIcon = document.querySelector('.toggle_btn i');
     const dropDownMenu = document.querySelector('.dropdown_menu');
 
-    toggleBtn.onclick = function () {
+    toggleBtn.onclick = function (event) {
+        event.stopPropagation();
         dropDownMenu.classList.toggle('open');
         const isOpen = dropDownMenu.classList.contains('open');
         toggleBtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
     };
 
-
-    // const menu = document.querySelector('header');
-    // let isSticky = false;
-
-    // function handleScroll() {
-    //     const shouldBeSticky = window.scrollY >= 1;
-
-    //     if (shouldBeSticky && !isSticky) {
-    //         menu.classList.add('fixed');
-    //         isSticky = true;
-    //     } else if (!shouldBeSticky && isSticky) {
-    //         menu.classList.remove('fixed');
-    //         isSticky = false;
-    //     }
-    // }
-
-});
-
-// ------------- Slider function --------------//
-
-let slides = document.getElementsByClassName("slides");
-let navlinks = document.getElementsByClassName("dot");
-let currentSlide = 0;
-
-document.getElementById("next").addEventListener("click", () => {
-    changeSlide(currentSlide + 1)
-});
-document.getElementById("prev").addEventListener("click", () => {
-    changeSlide(currentSlide - 1)
-});
-
-function changeSlide(moveTo) {
-    if (moveTo >= slides.length) {
-        moveTo = 0;
-    }
-    if (moveTo < 0) {
-        moveTo = slides.length - 1;
-    }
-
-    slides[currentSlide].classList.toggle("active");
-    navlinks[currentSlide].classList.toggle("activeDot");
-    slides[moveTo].classList.toggle("active");
-    navlinks[moveTo].classList.toggle("activeDot");
-    currentSlide = moveTo;
-}
-
-document.querySelectorAll(".dot").forEach((bullet, bulletIndex) => {
-    bullet.addEventListener("click", () => {
-        if (currentSlide !== bulletIndex) {
-            changeSlide(bulletIndex);
+    // Close dropdown menu when clicking outside toggle button
+    document.body.addEventListener('click', function (event) {
+        if (!event.target.closest('.dropdown_menu') && !event.target.closest('.toggle_btn')) {
+            dropDownMenu.classList.remove('open');
+            toggleBtnIcon.className = 'fa-solid fa-bars';
         }
     });
-});
 
-window.onload = setInterval(function () {
-    changeSlide(currentSlide + 1)
-}, 3000);
-
-
-// ------------- Work data function --------------//
-
-const workSection = document.querySelector('.section-work-data');
-const workObserver = new IntersectionObserver(
-    (entries, observer) => {
-        const [entry] = entries;
-
-        if (!entry.isIntersecting) return;
-
-        // animate number counter
-        const counterNum = document.querySelectorAll(".counter-numbers");
-        const speed = 200;
-
-        counterNum.forEach((curElem) => {
-            const updateNumber = () => {
-                const targetNumber = parseInt(curElem.dataset.number);
-                let currentNumber = parseInt(curElem.innerText);
-
-                const incrementNumber = Math.ceil(targetNumber / speed);
-
-                if (currentNumber < targetNumber) {
-                    currentNumber = Math.min(currentNumber + incrementNumber, targetNumber);
-                    curElem.innerText = currentNumber;
-                    setTimeout(updateNumber, 10);
-                }
-            };
-
-            updateNumber();
-        });
-
-        observer.unobserve(workSection);
-    },
-    {
-        root: null,
-        threshold: 0,
+    // Close dropdown menu on navigation events
+    document.body.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A' && event.target.closest('.navbar')) {
+            dropDownMenu.classList.remove('open');
+            toggleBtnIcon.className = 'fa-solid fa-bars';
+        }
     });
 
-workObserver.observe(workSection);
+    // ------------- Slider function --------------//
 
+    let slides = document.getElementsByClassName("slides");
+    let navlinks = document.getElementsByClassName("dot");
+    let currentSlide = 0;
+
+    document.getElementById("next").addEventListener("click", () => {
+        changeSlide(currentSlide + 1)
+    });
+    document.getElementById("prev").addEventListener("click", () => {
+        changeSlide(currentSlide - 1)
+    });
+    
+    function changeSlide(moveTo) {
+        if (moveTo >= slides.length) {
+            moveTo = 0;
+        }
+        if (moveTo < 0) {
+            moveTo = slides.length - 1;
+        }
+    
+        slides[currentSlide].classList.toggle("active");
+        navlinks[currentSlide].classList.toggle("activeDot");
+        slides[moveTo].classList.toggle("active");
+        navlinks[moveTo].classList.toggle("activeDot");
+        currentSlide = moveTo;
+    }
+    
+    document.querySelectorAll(".dot").forEach((bullet, bulletIndex) => {
+        bullet.addEventListener("click", () => {
+            if (currentSlide !== bulletIndex) {
+                changeSlide(bulletIndex);
+            }
+        });
+    });
+    
+    window.onload = setInterval(function () {
+        changeSlide(currentSlide + 1)
+    }, 3000);
+
+    // ------------- Work data function --------------//
+
+    const workSection = document.querySelector('.section-work-data');
+    const workObserver = new IntersectionObserver(
+        (entries, observer) => {
+            const [entry] = entries;
+
+            if (!entry.isIntersecting) return;
+
+            // animate number counter
+            const counterNum = document.querySelectorAll(".counter-numbers");
+            const speed = 200;
+
+            counterNum.forEach((curElem) => {
+                const updateNumber = () => {
+                    const targetNumber = parseInt(curElem.dataset.number);
+                    let currentNumber = parseInt(curElem.innerText);
+
+                    const incrementNumber = Math.ceil(targetNumber / speed);
+
+                    if (currentNumber < targetNumber) {
+                        currentNumber = Math.min(currentNumber + incrementNumber, targetNumber);
+                        curElem.innerText = currentNumber;
+                        setTimeout(updateNumber, 10);
+                    }
+                };
+
+                updateNumber();
+            });
+
+            observer.unobserve(workSection);
+        },
+        {
+            root: null,
+            threshold: 0,
+        });
+
+    workObserver.observe(workSection);
+
+});
